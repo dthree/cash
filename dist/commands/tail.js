0171821df8995ce61a85425ef72034a61843c3be
Dimitris Karittevlis <dimitris13k@gmail.com>
2016-03-02 21:48:36 +0000

    added few options. still need testing, help and refactoring

--- dist/commands/tail.js
+++ dist/commands/tail.js
@@ -2,56 +2,64 @@
 
 var interfacer = require('./../util/interfacer');
 var preparser = require('./../preparser');
+var path = require('path');
+var fs = require('fs');
+var glob = require("glob");
+
+var fsAutocomplete = require('vorpal-autocomplete-fs');
 
 var tail = {
   exec: function exec(args, options) {
     options = options || {};
 
-    this.log(args);
-    this.log(options);
+    options.lines = options.lines ? Math.abs(options.lines) : 10;
+    if (!Number.isInteger(options.lines)) {
+      this.log('tail: illegal offset -- ' + options.lines + ' \n');
+      return 0;
+    }
+
+    if (args != undefined) {
+      for (var i = 0; i < args.length; i++) {
+
+        var fileFound = false;
+        var isFile = false;
+
+        var pathToFile = path.resolve(process.cwd()).replace(/\\/g, '/') + '/' + args[i];
+        var allFiles = glob.sync(pathToFile);
+
+        for (var o = 0; o < allFiles.length; o++) {
+
+          try {
+            fs.accessSync(allFiles[o], fs.F_OK);
+            fileFound = true;
+          } catch (e) {
+            console.log('tail: ' + filePath + ': No such file or directory');
+          }
+
+          if (fileFound) {
+            isFile = fs.statSync(allFiles[o]).isFile();
+          }
+
+          if (isFile) {
+            var fileName = allFiles[0].replace(path.resolve(process.cwd()).replace(/\\/g, '/') + '/', '');
+            var fileContents = fs.readFileSync(allFiles[o], 'utf8');
+            var lines = fileContents.split("\n");
+
+            if ((args.length > 1 || allFiles.length > 1) && !options.silent || options.verbose) {
+              this.log('==> ' + fileName + ' <==');
+            }
+
+            for (var p = 0; p < lines.length; p++) {
+              if (p >= lines.length - 1 - options.lines) {
+                this.log(lines[p]);
+              }
+            }
+          }
+        }
+      }
+    }
 
     return 0;
-
-    /**
-     * 1. Validate arg input to include every
-     *    possible form of input, such as:
-     *
-     *    a. as a string:
-     *       cash.foo('bar and so on', {option: true});
-     *
-     *    b. as an array:
-     *       cash.foo(['bar', 'and', 'so', 'on'], {option: true});
-     *
-     *    c. as interpeted through Vorpal:
-     *       cash('foo bar and so on --option');
-     */
-
-    /**
-     * 2. For errors, use `this.log` to print the
-     *    error message to the user, and only return
-     *    the error code (1, 2, etc.)
-     *
-     *    if (args.length < 1) {
-     *      this.log('foo: cannot bar: requires at least one file name');
-     *      return 1;
-     *    }
-     */
-
-    /**
-     * 3. For all other stdout to the user, use `this.log`.
-     *    For large jobs that should stream logging a
-     *    bit at a time, use multiple `this.log` statements
-     *    and cash will handle the heavy lifting of piping
-     *    and returning the final assembled output to the
-     *    user in the case of programmatically executed
-     *    commands.
-     */
-
-    /**
-     * 4. When complete with no errors, return 0;
-     *
-     *    return 0;
-     */
   }
 };
 
@@ -60,7 +68,7 @@ module.exports = function (vorpal) {
     return tail;
   }
   vorpal.api.tail = tail;
-  vorpal.command('tail [files...]').parse(preparser).option('-n, --lines <number>', 'Output the last N lines, instead of the last 10').action(function (args, callback) {
+  vorpal.command('tail <files...>').parse(preparser).option('-n, --lines <number>', 'Output the last N lines, instead of the last 10').option('-q, --silent', 'Suppresses printing of headers when multiple files are being examined.').option('-v, --verbose', 'Always output headers giving file names.').autocomplete(fsAutocomplete()).action(function (args, callback) {
     args.options = args.options || {};
     return interfacer.call(this, {
       command: tail,
